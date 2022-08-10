f419604104424e2075c0694f94a8f25418c0f3d8
Abegail Pates <abegail.pates@gmail.com>
2022-08-10 13:30:47 +0800

    Refactor js code

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const text = document.querySelector('#untyped');
 const typedText = document.querySelector('#typed');
 const typedArray = [];
 const textArray = Array.from(text.textContent);
-console.log(textArray);
 
 window.addEventListener('keydown', checkKey);
 
@@ -10,21 +9,28 @@ function checkKey(e) {
     if (e.key === 'Shift') return;
 
     if (e.key === textArray[0]) {
-        console.log('match!');
-        textArray.shift();
-        typedArray.push(e.key);
+        playSound(true);
+        updateDisplay(e.key);
+    } else {
+        playSound(false);
+    }
+}
+
+function playSound(matched) {
+    if (matched) {
         const tink = document.querySelector('#tink');
         tink.currentTime = 0;
         tink.play();
-
-        text.textContent = textArray.join('');
-        typedText.textContent = typedArray.join('');
     } else {
-        console.log('not a match.')
         const boom = document.querySelector('#boom');
         boom.currentTime = 0;
         boom.play();
     }
-    console.log(textArray);
-    console.log(typedArray);
+}
+
+function updateDisplay(key) {
+    textArray.shift();
+    typedArray.push(key);
+    text.textContent = textArray.join('');
+    typedText.textContent = typedArray.join('');
 }
